79d96bb400d0ee7774fff1c1813c44aa2f47d8d9
KunMinX <kunminx@gmail.com>
2018-10-23 22:46:50 +0800

    add state utils

---
+++ baseSample/src/main/java/com/kunminx/viabus/utils/StatusUtils.java
@@ -0,0 +1,27 @@
+package com.kunminx.viabus.utils;
+
+/**
+ * @author KunMinX
+ * @date 2018/10/23
+ */
+public class StatusUtils {
+    public static boolean isEnabledStatus(int statuses, int... statusABC) {
+        boolean enabled = false;
+        for (int status : statusABC) {
+            enabled |= ((statuses & status) != 0);
+        }
+        return enabled;
+    }
+
+    public static int modifyStatus(int statuses, boolean add, int status) {
+        if (add) {
+            if ((statuses & status) == 0) {
+                statuses = statuses | status;
+            }
+        } else {
+            statuses = (statuses & ~status);
+        }
+        return statuses;
+    }
+}
+
